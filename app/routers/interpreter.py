d6cb8acf463e61b78787ddcb4f7c8e580d7c2245
MatiSrv <matisiravegna04@gmail.com>
2024-12-20 10:26:29 -0300

    avances

--- app/routers/interpreter.py
+++ app/routers/interpreter.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter,HTTPException
 from services.cohereService import get_cohere_response
 from models.dreamRequest import DreamRequest
+
+
 
 router = APIRouter(tags=["interpreter"], prefix="/interpreter")
 
 @router.post("/")
-async def interpret(dream_description: DreamRequest):    
+async def interpret(dream_description: DreamRequest):   
+    if not dream_description:
+        raise HTTPException(status_code=400, detail="No se ha proporcionado una descripción del sueño.")
     return get_cohere_response(dream_description.dream_description)
--- app/services/cohereService.py
+++ app/services/cohereService.py
@@ -58,7 +58,8 @@ tools = [
     },
 ]
 
-key_words = ['significado', 'significa', 'sueño', 'soñe','interpreta','soñar','significa','pesadilla', 'simbolismo', 'representa', 'relacion' ]
+key_words = ['significado', 'significa', 'sueño', 'soñe','interpreta','soñar','significa','pesadilla', 'simbolismo', 'representa', 'relacion','siento','emocion','emociones','enfrentar','miedo','ansiedad','preocupacion','alegria','tristeza'
+             ,'felicidad','sorpresa','enojo','ira','desesperacion','desesperanza','enfrento','siento','sentimiento','sintiendo','me','afrontar','mi','mis']
 
 instructions = """
 
